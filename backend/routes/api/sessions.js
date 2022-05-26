e7bb8c41ea584e221dc64e28a6e658f1a1849b0e
Israel Arvizu <israelarvizuofficial@gmail.com>
2022-05-26 16:44:07 -0700

    login validations added

--- backend/routes/api/sessions.js
+++ backend/routes/api/sessions.js
@@ -4,9 +4,22 @@ const asyncHandler = require('express-async-handler');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
 
+
+const validateLogin = [
+    check('credential')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email or username.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a password.'),
+    handleValidationErrors
+];
 //TO LOGIN /api/session/login POST REQUEST
-router.post('/login', asyncHandler(async (req, res, next) => {
+router.post('/login',validateLogin, asyncHandler(async (req, res, next) => {
     const {credential, password} = req.body;
 
     const user = await User.login({ credential, password});
