3863d7acdc2156389b2b2fd43a6a65cd3008c740
Israel Arvizu <israelarvizuofficial@gmail.com>
2022-05-26 16:59:43 -0700

    backend skeleton functionality completed

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -11,9 +11,7 @@ const { requireAuth } = require('../../utils/auth.js');
 router.use('/session', sessionRouter);
 router.use('/users', usersRouter);
 
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-});
+
 
 router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
     const user = await User.findOne({
