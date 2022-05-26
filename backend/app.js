fe679c3498c0d9225bfa5a74a1c26e46fb90d81e
Israel Arvizu <israelarvizuofficial@gmail.com>
2022-05-26 10:51:23 -0700

    working backend skeleton

---
+++ backend/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+
+const {enviorment} = require('./config');
+const isProduction = enviorment === 'production';
+const routes = require('./routes')
+
+const app = express();
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(express.json());
+
+if(!isProduction){
+    app.use(cors());
+}
+
+app.use(
+    helmet.crossOriginResourcePolicy({
+      policy: "cross-origin"
+    })
+  );
+
+app.use(
+    csurf({
+        cookie: {
+        secure: isProduction,
+        sameSite: isProduction && "Lax",
+        httpOnly: true
+        }
+    })
+);
+
+app.use(routes);
+
+
+
+module.exports = app;
