977c04468bb0baa1539f7a90dc2f6c1f2daacfb1
Israel Arvizu <israelarvizuofficial@gmail.com>
2022-05-27 16:59:00 -0700

    REDUX login functionality

---
+++ frontend/src/store/session.js
@@ -0,0 +1,56 @@
+import { csrfFetch } from "./csrf";
+
+const SET_USER = 'session/USER';
+const REMOVE_USER = 'session/REMOVE_USER';
+
+
+//ACTIONS
+const setUser = (user) => {
+    return {
+        type: SET_USER,
+        payload: user
+    }
+}
+
+const removeUser = () => {
+    return {
+        type: REMOVE_USER,
+    }
+}
+
+//THUNKERS
+export const login = (user) => async (dispatch) => {
+    const { credential, password } = user;
+    const response = await csrfFetch('/api/session/login', {
+        method: 'POST',
+        body: JSON.stringify({
+            credential,
+            password,
+        }),
+    });
+    const data = await response.json();
+    console.log(data);
+    dispatch(setUser(data));
+    return response;
+}
+
+
+//REDUCER
+const initialState = { user: null};
+const sessionReducer = (state= initialState, action) => {
+    let newState;
+    switch(action.type){
+        case SET_USER:
+            newState = Object.assign({}, state);
+            newState.user = action.payload;
+            return newState;
+        case REMOVE_USER:
+            newState = Object.assign({}, state);
+            newState.user = null;
+            return newState;
+        default:
+            return state;
+    }
+};
+
+export default sessionReducer;
