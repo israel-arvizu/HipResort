aa1fb042fbcda43f1bcc750f97f2797cb5039099
Israel Arvizu <israelarvizuofficial@gmail.com>
2022-06-05 14:42:08 -0700

    added allresorts styling and other minor css

--- frontend/src/components/Resorts/allResortsPage.js
+++ frontend/src/components/Resorts/allResortsPage.js
@@ -1,6 +1,8 @@
 import { useEffect }  from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import * as resortActions from '../../store/resort';
+import './allResortsPage.css'
+
 function ResortsPage(){
     const dispatch = useDispatch();
 
@@ -13,27 +15,52 @@ function ResortsPage(){
 
     return(allResorts.resorts &&
         <div className="resorts-main-container">
+            <div>
+            <hr></hr>
             <h2> All Resorts </h2>
-            <ul>
-                {allResorts.resorts.map((resort) => {
-                    return(
-                        <a href={`/resort/${resort.id}`}>
-                            <li key={resort.id}>
-                                <div className='resorts-container-sec'>
-                                    <img src={resort.imageUrl} alt="Resort Image"></img>
-                                    <span>{resort.name}</span>
-                                    <ul>
-                                        <li>{resort.price} per night</li>
-                                        <li>Capacity: {resort.capacity}</li>
-                                        <li>{resort.city}, {resort.state}</li>
-
-                                    </ul>
+            <hr></hr>
+            <div className='resorts-container-holder'>
+                <ul>
+                    <div className='resorts-container-body'>
+                    {allResorts.resorts.map((resort) => {
+                        return(
+                            <a className='resort-image-interactive' href={`/resort/${resort.id}`}>
+                                <div className='resort-info-container'>
+                                    <li key={resort.id}>
+                                        <div className='resorts-container-sec'>
+                                            <div className='resorts-image-container'>
+                                                <img className='resort-list-image' src={resort.imageUrl} alt="Resort"></img>
+                                            </div>
+                                            <div className='resorts-details-containers'>
+                                                <div>
+                                                    <span className='resort-details-name'>{resort.name}</span>
+                                                    <span><i class="fa-solid fa-circle-check fa-sm"></i></span>
+                                                </div>
+                                                <div className='resort-details-info-container'>
+                                                    <div className='resort-details-info-text'>
+                                                        <span>{resort.capacity} people </span>
+                                                        <span> - </span>
+                                                        <span>{resort.city}</span>
+                                                        <span> - </span>
+                                                        <span>{resort.state}</span>
+                                                    </div>
+                                                    <div>
+                                                        <span className='resorts-details-price-tag'>${resort.price}</span>
+                                                        <span className='resorts-details-price-tag-text'>/night</span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <hr className='resort-details-break'></hr>
+                                        </div>
+                                    </li>
                                 </div>
-                            </li>
-                        </a>
-                    )
-            })}
-            </ul>
+                            </a>
+                        )
+                    })}
+                    </div>
+                </ul>
+            </div>
+            </div>
         </div>
     )
 }
