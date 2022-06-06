4bdc1cfaba347c6687eea5a515501abccb099b3a
Israel Arvizu <israelarvizuofficial@gmail.com>
2022-06-06 00:48:03 -0700

    Css resort single page updated

--- frontend/src/components/Resorts/resortDetailsPage.js
+++ frontend/src/components/Resorts/resortDetailsPage.js
@@ -52,35 +52,57 @@ function ResortDetails() {
                     <div className='main-body-image-container'>
                         <img className='main-body-image' src={resort.imageUrl} alt='Resort Image'></img>
                     </div>
-                    <div className='content-container-main'>
+                    <div className='details-main-container-outer'>
                         <div>
-                            <h2 id="content-main-title"> {resort.name} </h2>
-                            <span> {resort.city}, {resort.state} </span>
-                            <hr></hr>
+                            <div className='content-container-main'>
+                                <div>
+                                    <h2 id="content-main-title"> {resort.name} </h2>
+                                    <span className='content-main-location'> {resort.city}, {resort.state} </span>
+                                    <hr id='line-break-details'></hr>
+                                </div>
+                                <div id="content-main-details-container">
+                                    <div id='content-main-resort-host'>
+                                        <img className="host-details-image" src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/7e/Circle-icons-profile.svg/1024px-Circle-icons-profile.svg.png" />
+                                        <p id='content-main-resort-name-head'> Hosted by: </p>
+                                        <p id='content-main-resort-name'> {host.username} </p>
+                                    </div>
+                                    <div>
+                                        <span id='content-main-resort-details-head'>About the property: </span>
+                                        <p id='content-main-resort-details'> {resort.details}</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='bottom-amenities-section-container'>
+                                <p>Amenities: </p>
+                                    <div className='bottom-amenities-section-body'>
+                                        <ul className='bottom-amenities-section-ul'>
+                                            {amenities.map((amenitie) => {
+                                                return (
+                                                    <div className='amenities-details-container'>
+                                                        <li key={amenitie.id} className="amenities-details-list-item">
+                                                            <img id='amenities-image' src={amenitie.pictureUrl} alt="Amenitie picture"/>
+                                                            <p>{amenitie.name}</p>
+                                                        </li>
+                                                    </div>
+                                                )
+                                            })}
+                                        </ul>
+                                    </div>
+                            </div>
                         </div>
-                        <div id="content-main-details-container">
-                            <p id='content-main-resort-host'> HOST: {host.username} </p>
-                            <p id='content-main-resort-details'> {resort.details}</p>
+                        <div className='content-container-right'>
+                            <div className='content-container-price-section'>
+                                <h3 id='booking-container-price'> ${resort.price} </h3>
+                                <p id='booking-container-cap' >per night ({resort.capacity} guest) </p>
+                            </div>
+                            <div  className='booking-container-capacity'>
+                                <p id='booking-container-capacity-header'>Guests</p>
+                                <p id='booking-container-capacity-bottom'>Capacity: {resort.capacity} </p>
+                            </div>
+                            <div className='booking-container'>
+                                {sessionLinks}
+                            </div>
                         </div>
-                    </div>
-                    <div className='bottom-amenities-section'>
-                        <ul>
-                            {amenities.map((amenitie) => {
-                                return (
-                                    <li key={amenitie.id}>
-                                        {amenitie.name}
-                                        <img src={amenitie.pictureUrl} alt="Amenitie picture"/>
-                                    </li>
-                                )
-                            })}
-                        </ul>
-                    </div>
-                </div>
-                <div className='content-container-right'>
-                    <h3> ${resort.price} </h3>
-                    <span>Capacity: {resort.capacity} </span>
-                    <div className='booking-container'>
-                        {sessionLinks}
                     </div>
                 </div>
             </div>
